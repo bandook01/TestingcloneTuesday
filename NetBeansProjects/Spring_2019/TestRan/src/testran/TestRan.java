296cc24273f59713b3f12be1a7e93b5180338df1
sivagamasheridan <sivagama.srinivasan@sheridanc.on.ca>
2019-05-20 16:49:40 -0400

    /*  * To change this license header, choose License Headers in Project Properties.  * To change this template file, choose Tools | Templates  * and open the template in the editor.  */ package test;
    /**
     *
     * @author kumarayyaduraigunaseelan
     */
    public class Test {
    
        /**
         * @param args the command line arguments
         */
        public static void main(String[] args) 
        {
        System.out.print("jkhnjk");    // TODO code application logic here
        }
        
    }

--- NetBeansProjects/Spring_2019/TestRan/src/testran/TestRan.java
+++ NetBeansProjects/Spring_2019/TestRan/src/testran/TestRan.java
@@ -13,7 +13,7 @@ public class TestRan
 {
 
  private String suit;
- public static final String SUITS[] = {"Diamonds","Spades","Clubs","Hearts"};
+ public static final String SUITS[] = {"Diamonds","Spades","Clubs","Hearts1345"};
  private int value;
 
     /**

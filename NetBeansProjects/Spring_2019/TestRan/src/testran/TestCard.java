beaf5bd368a052e771312319b399be05ed268689
sivagamasheridan <sivagama.srinivasan@sheridanc.on.ca>
2019-05-20 14:43:01 -0400

    /*  * To change this license header, choose License Headers in Project Properties.  * To change this template file, choose Tools | Templates  * and open the template in the editor.  */ package test;
    /**
     *
     * @author kumarayyaduraigunaseelan
     */
    public class Test {
    
        /**
         * @param args the command line arguments
         */
        public static void main(String[] args) 
        {
        System.out.print("jkhnjk");    // TODO code application logic here
        }
        
    }

---
+++ NetBeansProjects/Spring_2019/TestRan/src/testran/TestCard.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testran;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author kumarayyaduraigunaseelan
+ */
+public class TestCard
+{
+    public static void main(String args[])
+    {
+        TestRan[] Hand = new TestRan[7];
+        System.out.println(" 7 cards generated: \n");
+        
+       for(int i=0;i<Hand.length;i++)
+       {
+        TestRan c1= new TestRan();
+        c1.setSuit(TestRan.SUITS[c1.getRandomSuit()]);
+        c1.setValue(c1.getRandomValue());
+        Hand[i]= c1;
+        System.out.println(Hand[i].getSuit() + "\n" + Hand[i].getValue());
+        
+       }
+//input from user lucky card.
+
+Scanner in = new Scanner(System.in);
+System.out.println("enter suit");
+String userSuit = in.nextLine();
+//in.nextLine();
+System.out.println("enter value");
+int userValue= in.nextInt();
+//System.out.println(" \n");
+
+boolean found = false;
+for(int i=0;i<Hand.length;i++)
+{
+if(userValue==Hand[i].getValue() && userSuit.equalsIgnoreCase(Hand[i].getSuit()))
+{
+    found =true;
+    break;
+}  
+}
+
+if(found)
+{
+    System.out.println(" your matching card found");
+    
+}
+else
+{
+    System.out.println(" your matching card not found");
+}
+}
+}
---
+++ NetBeansProjects/Spring_2019/TestRan/src/testran/TestRan.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testran;
+
+/**
+ *
+ * @author kumarayyaduraigunaseelan
+ */
+public class TestRan
+{
+
+ private String suit;
+ public static final String SUITS[] = {"Diamonds","Spades","Clubs","Hearts"};
+ private int value;
+
+    /**
+     * @return the suit
+     */
+    public String getSuit() {
+        return suit;
+    }
+
+    /**
+     * @param suit the suit to set
+     */
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    /**
+     * @return the value
+     */
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(int value) {
+        this.value = value;
+    }
+//add two methods to generate random suit and values
+
+public int getRandomValue()
+{
+    int number= (int) (Math.random()*13+1);
+    return number;
+}
+public int getRandomSuit()
+{
+    int number = (int) (Math.random()*3+0);
+    return number;
+}
+        
+
+
+}
